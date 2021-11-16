7ad30c13555e8d7193c783fdc5f23055cc188dea
evs <evstigney2010@gmail.com>
2021-11-16 14:28:36 +0300

    Решение задачи trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -6,4 +6,19 @@
  */
 export function trimSymbols(string, size) {
 
+	if (size !== 0 && !size) return string;
+
+	let check = [];
+	let stringArr = [...string];
+
+	for (let i = 0; i < stringArr.length; i++) {
+
+		if (check[0] !== stringArr[i]) 	check = [];
+
+		if (check.length < size) check.push(stringArr[i]);
+		else stringArr.splice(i--, 1);
+
+	}
+
+	return stringArr.join('');
 }
