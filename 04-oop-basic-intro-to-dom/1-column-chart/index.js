cb6ef13b1f58cd4c1e8c94e51ce9c3476809e592
evs <evstigney2010@gmail.com>
2021-11-23 20:30:48 +0300

    Решение задачи ColumnChart компонент

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,69 @@
 export default class ColumnChart {
+	chartHeight = 50;
+
+	constructor ( content = {} ) {
+		this._content = content;
+		this._elem = this._render();
+	}
+
+	get element () {
+		return this._elem;
+	}
+
+	update (dataArr) {
+
+		if (!dataArr || !dataArr.length) {
+			this._elem.classList.add('column-chart_loading');
+			return;
+		}
+
+		const maxValue = Math.max(...dataArr);
+		const scale = this.chartHeight / maxValue;
+
+		this._elem.querySelector('[data-element="body"]').innerHTML = dataArr
+			.map((item) => `<div style="--value: ${Math.floor(item * scale)
+				}" data-tooltip="${(item / maxValue * 100).toFixed(0)}%"></div>`)
+			.join('');
+	}
+
+	remove () {
+		this._elem.remove();
+		this._elem = null;
+	}
+
+	destroy () {}
+
+	_render () {
+		let {
+			label = '',
+			link = '',
+			value = 0,
+			data, formatHeading } = this._content;
+		const markup = `
+			<div class="column-chart" style="--chart-height: ${this.chartHeight}">
+				<div class="column-chart__title">
+					${label}
+					<a href="${link}" class="column-chart__link">View all</a>
+				</div>
+				<div class="column-chart__container">
+					<div data-element="header" class="column-chart__header">${
+						(formatHeading) ? formatHeading(value) : value
+					}</div>
+					<div data-element="body" class="column-chart__chart">
+					</div>
+				</div>
+			</div>
+			`.trim();
+		this._elem = this._createElement(markup);
+		this.update(data);
+
+		return this._elem;
+	}
+
+	_createElement (html) {
+		const elem = document.createElement('div');
+		elem.innerHTML = html;
+		return elem.firstElementChild;
+	}
 
 }
